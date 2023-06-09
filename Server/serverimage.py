472b982aad21f191e802d4630c8905e8fafbb113
Airosspace R&D Private Limited <67426531+Airosspace@users.noreply.github.com>
2023-06-09 12:20:19 +0530

    Update and rename serverimage.txt to serverimage.py

---
+++ Server/serverimage.py
@@ -0,0 +1,51 @@
+import socket
+import struct
+
+def receive_image(save_path, server_ip, server_port):
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        
+        server_socket.bind((server_ip, server_port))
+
+        
+        server_socket.listen(1)
+        print("Server is listening on {}:{}".format(server_ip, server_port))
+
+        
+        client_socket, client_address = server_socket.accept()
+        print("Connected to client:", client_address)
+
+        
+        size_data = client_socket.recv(4)
+        image_size = struct.unpack("!I", size_data)[0]
+
+        
+        image_data = b""
+        remaining_bytes = image_size
+        while remaining_bytes > 0:
+            chunk = client_socket.recv(min(4096, remaining_bytes))
+            if not chunk:
+                break
+            image_data += chunk
+            remaining_bytes -= len(chunk)
+        with open(save_path,"wb") as file:
+            file.write(image_data)
+        print("Image received and saved successfully")
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        
+        server_socket.close()
+        print("Server socket closed")
+i=1
+while(0<1):
+    save_path = f"rimages/received_image{i}.jpeg"
+    #server_ip = "192.168.92.246" 
+    server_ip="172.16.4.152" 
+    server_port = 5500 
+
+
+    receive_image(save_path, server_ip, server_port)
+    i+=1
